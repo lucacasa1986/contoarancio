04f9c6a39692926563eb1dea339ca84ae7cf9900
Luca Casarotti <luca.casarotti@dedalus.eu>
2018-03-19 17:27:52 +0100

    dai

--- bilancio/parse_excel.py
+++ bilancio/parse_excel.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DATABASE=os.path.join('bilancio/flaskr.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='default',
