c6244566858e2cbb37c8c88fbcb54ca7363cd1cb
Luca Casarotti <luca.casarotti@dedalus.eu>
2018-03-20 15:58:34 +0100

    schema db

--- bilancio/schema.sql
+++ bilancio/schema.sql
@@ -9,6 +9,24 @@ create table categorie
 )
 ;
 
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (1, 'Trasporti', '#EC671A', 'fa fa-plane');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (2, 'Utenze', '#FF69B8', 'fa fa-bath');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (3, 'Assicurazioni e Finanziamenti', '#21A5BA', 'fa fa-umbrella');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (4, 'Bambini', '#5E35B2', 'fa fa-child');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (5, 'Animali domestici', '#558B2C', 'fa fa-paw');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (6, 'Casa', '#558B2F', 'fa fa-home');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (7, 'Scuola', '#3E2723', 'fa fa-book');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (8, 'Medicine e spese sanitarie', '#006B61', 'fa fa-medkit');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (9, 'Shopping', '#283593', 'fa fa-shopping-bag');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (10, 'Cura della persona', '#CC99FF', 'fa fa-user-circle');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (11, 'Tempo libero e viaggi', '#87BAE4', 'fa fa-glass');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (12, 'Cibo e spese', '#A6339D', 'fa fa-shopping-cart');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (13, 'Altre spese', '#616161', 'fa fa-money');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (14, 'Investimenti', '#9C9D43', 'fa fa-bar-chart');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (15, 'Patrimonio', '#F9A825', 'fa fa-diamond');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (16, 'Contabilita', '#558B4E', 'fa fa-dollar');
+INSERT INTO categorie (id, descrizione, colore, icon_class) VALUES (17, 'Tasse e sanzioni', '#5E35B1', 'fa fa-legal');
+
 create table conti
 (
 	id INTEGER
